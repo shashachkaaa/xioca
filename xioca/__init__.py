2c7701d145253d3967b72d5b10ebd9c1e1f0dcb3
shashachkaaa <130691267+shashachkaaa@users.noreply.github.com>
2025-12-25 11:59:25 +0500

    Bump version to 2.7.0 and update changelog
    Updated version to 2.7.0 and modified update description.

--- xioca/__init__.py
+++ xioca/__init__.py
@@ -11,19 +11,16 @@ __authors__ = "Sh1tN3t - https://github.com/sh1tn3t https://t.me/sh1tn3t | shash
 __license__ = "GNU Affero General Public License v3.0"
 __copyright__ = "Copyright (C) 2020-2021 Sh1tN3t"
 
-__version__ = "2.6.9"
+__version__ = "2.7.0"
 __start_time__ = datetime.now()
 __system_mod__ = ["loader", "help", "tester", "updater", "information", "executor", "settings", "terminal", "info", "botmanager", "eval", "evaluator"]
 __get_version_url__ = "https://raw.githubusercontent.com/shashachkaaa/xioca/refs/heads/main/xioca/__init__.py"
 __get_commits_url__ = f"https://api.github.com/repos/shashachkaaa/xioca/commits"
 
-__update_desc__ = """Обновлен botmanager: Теперь бот запросит у вас разрешение на создании папки "Xioca" в которой будут содержатся следующие чаты/каналы:
-• Xioca SUPPORT 
-• Xioca OFFTOP
-• Xioca - New top pyrogram userbots
-• Ваш инлайн бот (Xioca of ...)
-
-Исправлены ошибки.
-
-Обновлен eval:
-Красивое оформление, убран весь мусор при выводе ошибок."""
+__update_desc__ = """🌐 Локализация
+Добавлена поддержка нескольких языков. Сменить язык можно командой .setlang.
+📖 Разработчикам
+Документация доступна по ссылке: xioca.ferz.live/docs
+🐛 Багфиксы
+Исправлена ошибка 400 peer_id_invalid, из-за которой сообщение не редактировалось после перезагрузки.
+"""
