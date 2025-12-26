7c9fc22661031dbcb97b3ef8ede2a5e239cd1528
shashachkaaa <130691267+shashachkaaa@users.noreply.github.com>
2025-12-26 14:46:23 +0500

    Update __init__.py

--- xioca/__init__.py
+++ xioca/__init__.py
@@ -11,16 +11,11 @@ __authors__ = "Sh1tN3t - https://github.com/sh1tn3t https://t.me/sh1tn3t | shash
 __license__ = "GNU Affero General Public License v3.0"
 __copyright__ = "Copyright (C) 2025 shashachkaaa"
 
-__version__ = "2.7.0"
+__version__ = "2.7.1"
 __start_time__ = datetime.now()
 __system_mod__ = ["loader", "help", "tester", "updater", "information", "executor", "settings", "terminal", "info", "botmanager", "eval", "evaluator"]
 __get_version_url__ = "https://raw.githubusercontent.com/shashachkaaa/xioca/refs/heads/main/xioca/__init__.py"
 __get_commits_url__ = f"https://api.github.com/repos/shashachkaaa/xioca/commits"
 
-__update_desc__ = """🌐 Локализация
-Добавлена поддержка нескольких языков. Сменить язык можно командой .setlang.
-📖 Разработчикам
-Документация доступна по ссылке: xioca.ferz.live/docs
-🐛 Багфиксы
-Исправлена ошибка 400 peer_id_invalid, из-за которой сообщение не редактировалось после перезагрузки.
-"""
+__update_desc__ = """В юзербот Xioca добавлено несколько новых способов авторизации: QR-код и Web-панель (это удобно, быстро и безопасно). Также исправлен баг в модуле help.
+Системные логги переведены на английский язык."""
